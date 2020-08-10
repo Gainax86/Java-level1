3f7a8eedd7a7424635d49db9e71ee0ffcfdf4d78
Gainax86 <Gainax86@gmail.com>
2020-08-10 22:05:49 +0300

    Домашние задание на java

---
+++ HomeWork.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.j_one.lesson_a.online;
+
+import java.awt.geom.Arc2D;
+
+public class HomeWork {
+
+        public static float calcul (int a, int b, int c, int d){
+            float d2= d;
+            return  a * (b + (c / d2));
+
+    }
+}
